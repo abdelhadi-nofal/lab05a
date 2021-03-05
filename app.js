2ca4ac052c688bf42a870cc315d91851c29fddd2
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-03-05 13:44:57 +0200

    problem2_solved

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function Sum(a, b) {
 }
 
 // Here is the test for sum(); uncomment it to run it
-console.log(Sum(4, 7));
+Sum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -30,12 +30,16 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a, b) { //eslint-disable-line
+function multiply(a, b) { 
+    let multi=a*b
+    let arr=[multi,'the product of  '+a +' and  ' + b]
+    console.log(arr);
+    return;
 
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+multiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
